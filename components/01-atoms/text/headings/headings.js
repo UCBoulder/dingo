8a4105c7fe84dd43acfe5bd922d9dca68a3ccdec
protitude <miles.france@gmail.com>
2023-08-23 16:17:36 +0000

    h1: add linear background to heading background

--- components/01-atoms/text/headings/headings.js
+++ components/01-atoms/text/headings/headings.js
@@ -5,7 +5,7 @@
   $("h2:not(:has(>a))").wrapInner('<span></span>');
   if ( $(".h1[data-bg-image]").length ) {
     var image = $('h1').attr('data-bg-image');
-    $('head').append('<style>h1:after{background-image: url(' + image + ')};</style>');
+    $('head').append('<style>h1{color:#fff !important;} h1:before{background-image: linear-gradient(180deg, rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.8) 90%);} h1:after{background: url(' + image + ') no-repeat center};</style>');
   }
 
   var mainSidebar = document.getElementsByClassName('main-sidebar');
