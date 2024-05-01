fd9b4428673b65a78550c71cc61bbc3c00255f33
protitude <miles.france@gmail.com>
2024-05-01 13:23:17 -0600

    refactor sections to fix sidebar

--- components/01-atoms/text/headings/headings.js
+++ components/01-atoms/text/headings/headings.js
@@ -8,25 +8,4 @@
     $('head').append('<style>h1{color:#fff !important;} h1:before{background-image: linear-gradient(180deg, rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.8) 90%);} h1:after{background: url(' + image + ') no-repeat center};</style>');
   }
 
-  var mainSidebar = document.getElementsByClassName('main-sidebar');
-  var nodeNews = document.getElementsByClassName('page-node-type-news');
-  var nodeTabs = document.getElementById('block-dingo-local-tasks');
-  if (mainSidebar.length > 0 && nodeNews.length === 0) {
-    setTimeout( function() {
-      if (mainSidebar[0].offsetTop < 500) {
-        var responsivePageWrapper = document.getElementsByClassName('responsive-menu-page-wrapper');
-        var cassowaryMain = document.getElementById("block-cassowary-system-main");
-        // Subtract main content area from overall content wrapper to get distance to top.
-        var distanceToTop = cassowaryMain.offsetTop - responsivePageWrapper[0].offsetTop;
-        var responsivePageWrapper = document.getElementsByClassName('block-pageoverviewblock');
-        var offsetAdmin = (nodeTabs) ? 29 : 0;
-        var offsetTweak = (nodeNews.length === 0) ? -139 : -185;
-        var styleMarginTop = distanceToTop + offsetTweak + offsetAdmin;
-        styleMarginTop += "px";
-
-        mainSidebar[0].style.marginTop = styleMarginTop;
-      }
-    }, 100);
-  }
-
 })(jQuery, Drupal);
