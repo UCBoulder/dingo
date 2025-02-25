e1a24d1a03e39637fcce6de02aa726e25cc43373
protitude <miles.france@gmail.com>
2025-02-25 16:27:12 -0700

    details
    Add facets details style

--- components/01-atoms/details/details.js
+++ components/01-atoms/details/details.js
@@ -3,20 +3,27 @@
   "use strict";
 
     var dtn = $( ".main-content details" ).length;
+
     function detailsControls(placement) {
       $(placement).before('<div class="detail-control"><button class="button-internal open-deets">Open All</button> <button class="button-internal button-internal--active close-deets">Collapse All</button></div>');
     }
+
     if ( dtn > 2 && !$('details').hasClass("no-deets-controls") && !$('div').hasClass("deets-controls-here") ) {
       detailsControls('.main-content details:first');
     }
+
     if ( $('div').hasClass("deets-controls-here") ) {
       detailsControls(".deets-controls-here");
     }
+
     $('button.open-deets').click(function() {
       $('details').attr('open', '');
     });
+
     $('button.close-deets').click(function() {
       $('details').removeAttr('open');
     });
+
+    $('html:has(main .view-facets) .views-exposed-form details').attr('open', 'open');
 
 })(jQuery, Drupal);
