884fd75b0aff7523a22f566886bcceec67401604
protitude <miles.france@gmail.com>
2025-03-03 13:48:17 -0700

    Style tweaks from /UCBoulder/oit_dingo#847

--- components/01-atoms/buttons/buttons.js
+++ components/01-atoms/buttons/buttons.js
@@ -1,22 +1,36 @@
-(function($, Drupal) {
-  $( document ).ready(function() {
-    $('.button-internal').click(function() {
-      internalButtonSelected(this);
-    });
-    function internalButtonSelected(thisClass) {
-      $('.button-internal--active span').remove();
-      $('.button-internal--active').removeClass('button-internal--active');
-      $(thisClass).addClass('button-internal--active').append('<span><svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg></span>');
-      gsap.from(".checkmark__check", {duration:.4, drawSVG: "0"});
-      $('.button-internal').attr('aria-selected', 'false');
-      $(thisClass).attr("aria-selected", "true");
-    }
-    setTimeout(function() {
-      if ( $(".button-internal").hasClass('button-internal--active') ) {
-        $('.button-internal--active').addClass('clkemlkrjwelrkjewnce-temp');
-        internalButtonSelected('.clkemlkrjwelrkjewnce-temp');
-        $('.clkemlkrjwelrkjewnce-temp').removeClass('clkemlkrjwelrkjewnce-temp');
+(function ($, Drupal) {
+  Drupal.behaviors.internalButtons = {
+    attach: function (context, settings) {
+      const self = this;
+
+      // Run initialization for both initial load and AJAX updates
+      initializeButtons(context);
+
+      // Define internalButtonSelected as a property of the behavior
+      this.internalButtonSelected = function (thisClass) {
+        $('.button-internal--active span').remove();
+        $('.button-internal--active').removeClass('button-internal--active');
+        $(thisClass).addClass('button-internal--active').append('<span><svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg></span>');
+        $('.button-internal').attr('aria-selected', 'false');
+        $(thisClass).attr("aria-selected", "true");
+        gsap.from(".checkmark__check", { duration: .4, drawSVG: "0", delay: 0.2 });
+      };
+
+      // Find all .button-internal elements and attach click handlers
+      $(once('internalButtons', '.button-internal', context)).each(function () {
+        $(this).on('click', function (e) {
+          self.internalButtonSelected(this);
+        });
+      });
+
+      function initializeButtons(context) {
+        // Find active buttons within the current context
+        $('.button-internal--active', context).each(function () {
+          if (!$(this).find('span.checkmark').length) {
+            self.internalButtonSelected(this);
+          }
+        });
       }
-    }, 2000);
-  });
+    }
+  };
 })(jQuery, Drupal);
