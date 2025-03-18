e9b40fe107653c385ee8a3591937290a9c5423a2
protitude <miles.france@gmail.com>
2025-03-18 16:16:57 -0600

    headings: remove span getting added via js

--- components/01-atoms/text/headings/headings.js
+++ components/01-atoms/text/headings/headings.js
@@ -2,7 +2,6 @@
 
   "use strict";
   document.querySelector("h2");
-  $("h2:not(:has(>a))").wrapInner('<span></span>');
   if ( $(".h1[data-bg-image]").length ) {
     var image = $('h1').attr('data-bg-image');
     $('head').append('<style>h1{color:#fff !important;} h1:before{background-image: linear-gradient(180deg, rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.8) 90%);} h1:after{background: url(' + image + ') no-repeat center};</style>');
